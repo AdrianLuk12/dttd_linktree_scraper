a034f2c72849d6f027a10615fe8176bb593ae3a4
AdrianLuk12 <adrian.luk@dttd.io>
2024-07-16 12:03:29 +0800

    updated datetime and output

--- convert_unix_time.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-# Convert the timestamp to seconds
-timestamp_seconds = 1636288616000 / 1000
-
-# Convert the timestamp to a datetime object
-date_time = datetime.datetime.fromtimestamp(timestamp_seconds)
-
-print(date_time)
